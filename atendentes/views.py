a72ca6ffbd5e2e4121a4c8ebf99352916ed7ffef
Brenno Santos <brennol.dev@gmail.com>
2025-08-23 16:05:57 -0300

    adicionado sistema de autenticação e registro aos atendentes

--- atendentes/views.py
+++ atendentes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.views import View
 # Create your views here.
+
+class ChamadosView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'atendentes/chamados.html')
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        if not request.user.is_authenticated:
+            return HttpResponseRedirect('/autenticacao/login/')
+
+        return render(request, 'index.html')
